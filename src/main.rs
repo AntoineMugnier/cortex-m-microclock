bbb752a92f62a83cfb5c65a5d04f0e65b6f30c13
Antoine Mugnier <antoine.mugnier@depixus.com>
2023-06-11 16:15:26 +0200

    defmt-rtt added to project

--- src/main.rs
+++ src/main.rs
@@ -2,36 +2,38 @@
 #![no_std]
 use kaori_hsm::*;
 use cortex_m_rt::entry;
-use cortex_m_semihosting::{debug, hprintln};
-mod hsm;
 use hsm::{BasicStateMachine, BasicEvt};
-use panic_halt as _;
+mod hsm;
 
 use stm32f1::stm32f103;
 use cortex_m::peripheral;
+
+use defmt_rtt as _; // global logger
+use panic_probe as _;
+
 #[entry]
 fn main() -> ! {
 
-    hprintln!("hello world").unwrap();
+    defmt::println!("hello,  world !");
    
-    // let mut cp = cortex_m::Peripherals::take().unwrap();
-   // let mut peripherals = stm32f103::Peripherals::take().unwrap();
+    let mut cp = cortex_m::Peripherals::take().unwrap();
+    let mut peripherals = stm32f103::Peripherals::take().unwrap();
 
-   // //Configure main clock
-   // let mut rcc = peripherals.RCC;
-   // let tim2 = &peripherals.TIM2;
+    //Configure main clock
+    let mut rcc = peripherals.RCC;
+    let tim2 = &peripherals.TIM2;
 
-   // let basic_state_machine = BasicStateMachine::new();
-   // 
-   // let mut sm = StateMachine::from(basic_state_machine);
+    let basic_state_machine = BasicStateMachine::new();
+    
+    let mut sm = StateMachine::from(basic_state_machine);
 
-   // sm.init();
+    sm.init();
 
-   // let evt_list = [BasicEvt::A, BasicEvt::B];
+    let evt_list = [BasicEvt::A, BasicEvt::B];
 
-   // for evt in evt_list {
-   //     sm.dispatch(&evt);
-   // }
+    for evt in evt_list {
+        sm.dispatch(&evt);
+    }
 
     loop{}
 }
