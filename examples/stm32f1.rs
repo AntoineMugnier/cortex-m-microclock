eb84c66ea54cdd776bcaad071020f6871dee68cf
Antoine Mugnier <antoine.mugnier@depixus.com>
2023-06-22 22:07:56 +0200

    Have added concurrent access to microclock in example, lib simplified

--- examples/stm32f1.rs
+++ examples/stm32f1.rs
@@ -3,11 +3,26 @@
 use cortex_m_rt::{entry, exception};
 
 use stm32f1::stm32f103;
-use cortex_m::peripheral;
+use cortex_m::peripheral::syst;
 
 use defmt_rtt as _; // global logger
 use panic_probe as _;
-use cortex_m_microclock::{CYCCNTClock, Microclock};
+use cortex_m_microclock::{CYCCNTClock, Instant, Duration};
+use cortex_m::interrupt::{self, Mutex};
+use core::{cell::RefCell, ops::DerefMut};
+
+const SYSCLK_FREQ_HZ : u32 = 8_000_000;
+static MICROCLOCK: Mutex<RefCell<Option<CYCCNTClock::<SYSCLK_FREQ_HZ>>>> =
+    Mutex::new(RefCell::new(None));
+
+fn micro_now() -> Instant<SYSCLK_FREQ_HZ>{
+   return interrupt::free(|cs|{
+   let  mut microclock =  MICROCLOCK.borrow(cs).borrow_mut();
+        return microclock.deref_mut().as_mut().unwrap().now();
+    }
+    );
+    
+}
 
 #[entry]
 fn main() -> ! {
@@ -16,6 +31,15 @@ fn main() -> ! {
        
     let mut cp = cortex_m::Peripherals::take().unwrap();
     let mut p = stm32f103::Peripherals::take().unwrap();
+
+
+    let mut systick = cp.SYST;
+    systick.set_clock_source(syst::SystClkSource::Core);
+    systick.set_reload(1_000);
+    systick.clear_current();
+    systick.enable_counter();
+    systick.enable_interrupt();
+
     let rcc = &mut p.RCC; 
    
     // Set 8mhz HSE as direct SYSCLK source    
@@ -35,22 +59,23 @@ fn main() -> ! {
     const SYSCLK_FREQ_HZ: u32 = 8_000_000;
 
     let mut microclock = CYCCNTClock::<SYSCLK_FREQ_HZ>::new(&mut dcb, dwt);
-    microclock.start();
-    let duration = <CYCCNTClock::<SYSCLK_FREQ_HZ> as Microclock>::Duration::secs(1);
+    interrupt::free(|cs| MICROCLOCK.borrow(cs).replace(Some(microclock)));
+
+    let duration = Duration::<SYSCLK_FREQ_HZ>::secs(1);
+
     while true {
-        let init_inst = microclock.now();
-        microclock.delay(duration);
-        let t = microclock.now() - init_inst;
+        let init_inst = micro_now();
+        let t = micro_now() - init_inst;
         defmt::info!("time_us {}", t.to_micros());
-    } 
-    defmt::info!("Start");
-    microclock.delay(duration);
-    defmt::info!("End");
+    }
 
     loop{}
 }
 
 #[exception]
 fn SysTick(){
-
+   interrupt::free(|cs|
+   if let Some(ref mut microclock) =  *MICROCLOCK.borrow(cs).borrow_mut(){
+            microclock.update();
+    });
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,62 +2,41 @@
 
 use cortex_m::peripheral::{DCB, DWT};
 
+pub type Instant<const TIMER_HZ: u32> = fugit::TimerInstantU64<TIMER_HZ>;
+pub type Duration<const TIMER_HZ: u32> = fugit::TimerDurationU64<TIMER_HZ>;
+ 
 pub struct CYCCNTClock<const SYSCLK_HZ: u32>{
     dwt: DWT,
     previous_cyccnt_val : u32,
     nb_cyccnt_cycles : u32,
 }
 
-
-pub trait Microclock{
-    type Instant;
-    type Duration;
-    
-    fn start(&mut self);
-    fn stop(&mut self);
-    fn reset(&mut self);
-    fn now(&mut self) -> Self::Instant;
-    fn delay(&mut self, duration: Self::Duration);
-}
-
-impl <const TIMER_HZ: u32> Microclock for CYCCNTClock<TIMER_HZ>{ 
-    type Instant = fugit::TimerInstantU64<TIMER_HZ>;
-    type Duration = fugit::TimerDurationU64<TIMER_HZ>;
-    
-    fn start(&mut self){
-        self.dwt.enable_cycle_counter();
-    }
-    
-    fn stop(&mut self) {
+impl <const SYSCLK_HZ :u32 >Drop for  CYCCNTClock<SYSCLK_HZ>{
+    fn drop(&mut self) {
         self.dwt.disable_cycle_counter();
     }
+}
 
-    fn now(&mut self) -> Self::Instant {
+impl <const SYSCLK_HZ :u32 > CYCCNTClock<SYSCLK_HZ>{
+
+   const MAX_CYCCNT_VAL: u32 = core::u32::MAX;
+   
+    pub fn now(&mut self) -> Instant<SYSCLK_HZ> {
         
         //Call `update()` because the CYCCNT counter could have wrapped around since the last time
         //`update()` was called
         self.update();
 
         let acc_cyccnt_val : u64 = (self.nb_cyccnt_cycles as u64) *(Self::MAX_CYCCNT_VAL as u64 +1) + (self.dwt.cyccnt.read() as u64);
-        Self::Instant::from_ticks(acc_cyccnt_val)
+        Instant::from_ticks(acc_cyccnt_val)
     }
 
-    fn reset(&mut self){
-       self.dwt.set_cycle_count(0);
-       self.previous_cyccnt_val = 0;
-       self.nb_cyccnt_cycles = 0;
-    }
-
-    fn delay(&mut self, duration: Self::Duration) {
+    pub fn delay(&mut self, duration: Duration<SYSCLK_HZ>) {
         let instant_init = self.now();
         while (self.now() - instant_init) < duration{
             // NOP
         }
     }
-}
-
-impl <const SYSCLK_HZ :u32 > CYCCNTClock<SYSCLK_HZ>{
-   const MAX_CYCCNT_VAL: u32 = core::u32::MAX;
 
    pub fn update(&mut self){
         
@@ -76,7 +55,7 @@ impl <const SYSCLK_HZ :u32 > CYCCNTClock<SYSCLK_HZ>{
         dcb.enable_trace();
         DWT::unlock();
         assert!(DWT::has_cycle_counter());
-
+        dwt.enable_cycle_counter();
         dwt.set_cycle_count(0);
 
         CYCCNTClock{
