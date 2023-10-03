2606a4ab29d877c1bab1d63ffcba66a651b42ae6
Antoine Mugnier <amugnier@europe.com>
2023-10-03 16:35:12 -0600

    Code simplified and doc at the head of the page updated

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,39 @@
-//! This crate provides a software clock (`CYCCNTClock`) allowing to measure time durations and produce delays in a 
-//! cortex-m core at a microsecond scale.
+//! This crate provides a software clock which relies on the CYCCNT counter present in most Cortex-M chips 
+//! to allow user to measure time and produce delays. The precision of the
+//! clock depends on your microcontroller core frequency. If you have a core running at at least
+//! 1MHZ, you will have a microsecond precision. 
 //! 
-//! ***Note***: Some Cortex-M cores like M0, M0+ and M23 cores does not have a CYCCNT counter.
+//! ***Note 0***: Some Cortex-M cores like M0, M0+ and M23 cores does not have a CYCCNT counter.
 //! Therefore, the present crate cannot be used on these chips
-//!
+//! ***Note 1*** The present crate does not work on multicore systems.
+//! 
 //! # Underlaying hardware
 //! The clock is based on the CYCCNT counter from the Cortex-M DWT peripheral, which increments
-//! with each SYSCLK tick. However as the CYCCNT upcounter is only 32 bits wide, it may overflow 
-//! quite rapidly depending on your SYSCLK frequency. The `CYCCNTClock` allows to keep track of 
-//! multiple CYCCNT cycles and can be used to evaluate very large durations of time.
+//! with each processor clock cycle. However as the CYCCNT upcounter is only 32 bits wide, it may overflow 
+//! quite rapidly depending on your SYSCLK frequency. The `CYCCNTClock` keeps track of 
+//! multiple CYCCNT cycles  using an internal counter so it can be used to evaluate very large durations of time.
 //!
-//! # The clock as a singleton
-//! The `CYCCNTClock` structure is a singleton. This implies that the structure must not be
-//! instanciated and that all its methods are static. Those methods can be called from any thread
-//! without concurrency issues.  
-//! This design patern allows minimal verbosity in configuring and using the clock.
+//! # Crate structure
+//! The `CYCCNTClock` is the structure representing the software clock. This structure is a
+//! singleton exposing all the methods of the crate available to user. All these methods are static and
+//! can be called from any thread without concurrency issues.    
 //! 
 //! # How to use this crate
-//! In order to use the clock you should first call the `init()` method which take ownership of
-//! the DWT peripheral. From this point you can use `now()` and `delay()` methods.
-//! The `update()` method should be called periodically to avoid missing 
-//! the CYCCNT wrapping around.
+//! In order to use the clock you should first call the [`CYCCNTClock::init()`] method which takes ownership of
+//! the DWT peripheral. From this point you can use [`CYCCNTClock::now()`] and [`CYCCNTClock::delay()`] methods.
+//! The [`CYCCNTClock::update()`] method should be called periodically to avoid missing 
+//! the CYCCNT wrapping around. Use the following equation to find the minimum update frequency:
+//!
+//! ```min_update_freq = SYS_CLK_FREQ/(2³²)```
+//!
+//! Note that the [`CYCCNTClock::now()`] and [`CYCCNTClock::delay()]` methods implicitely call the [`CYCCNTClock::update()`] method.
 //! 
+//! # Example
+//!  A complete example of the use of this crate on a STMF103 blue pill is featured in the `examples` directory of this
+//!  project.
+//!  
 //!# Credits
 //!  Many thanks to the authors of `fugit` and `rtic::dwt_systick_monotonic` crates
-//!
 //!
 
 #![no_std]
@@ -77,7 +86,7 @@ impl <const SYSCLK_HZ :u32 > CYCCNTClock<SYSCLK_HZ>{
        
           let nb_cyccnt_cycles : u32 = *NB_CYCCNT_CYCLES.borrow(cs).borrow();
           
-          if let Some(ref mut dwt) = DWT.borrow(cs).borrow_mut().deref_mut(){
+          if let Some(ref mut dwt) = DWT.borrow(cs).borrow_mut().deref_mut().as_mut(){
               let acc_cyccnt_val : u64 = (nb_cyccnt_cycles as u64) *(Self::MAX_CYCCNT_VAL as u64 +1) + (dwt.cyccnt.read() as u64);
               Instant::from_ticks(acc_cyccnt_val)
           }
@@ -112,20 +121,19 @@ impl <const SYSCLK_HZ :u32 > CYCCNTClock<SYSCLK_HZ>{
     /// Synchronize the hardware counter with this clock.
     /// Must be called at least one time for every CYCCNT counter cycle after init. Otherwise
     /// time counting will be corrupted.
-    /// Generally, calling this method in every SysTick IRQ call is the simpler option
+    /// In general, calling this method in every SysTick IRQ call is the simpler option
     /// This method will NOT panic if the `init()` method has not be called first.
    pub fn update(){
         
        interrupt::free( |cs|{
-            if let Some(ref mut dwt) = DWT.borrow(cs).borrow_mut().deref_mut(){
+            if let Some(ref mut dwt) = DWT.borrow(cs).borrow_mut().deref_mut().as_mut(){
                 let cyccnt_val = dwt.cyccnt.read();
                 
                 let mut previous_cyccnt_val = PREVIOUS_CYCCNT_VAL.borrow(cs).borrow_mut();
                 
                 // increment the number of counted CYCCNT cycles in case of CYCCNT counter overflow
                 if *previous_cyccnt_val as u32 > cyccnt_val {
-                    let mut nb_cyccnt_cycles = NB_CYCCNT_CYCLES.borrow(cs).borrow_mut();
-                    *nb_cyccnt_cycles+=1;
+                    NB_CYCCNT_CYCLES.borrow(cs).replace_with(|&mut old| old +1);
                 }
 
                 *previous_cyccnt_val = cyccnt_val as usize;
@@ -152,8 +160,7 @@ impl <const SYSCLK_HZ :u32 > CYCCNTClock<SYSCLK_HZ>{
             dwt.set_cycle_count(0);
             
             // Store the DWT peripheral into a shared object
-            let mut dwt_slot = DWT.borrow(cs).borrow_mut(); 
-            *dwt_slot.deref_mut() = Some(dwt);
+            DWT.borrow(cs).borrow_mut().replace(dwt); 
         });
 }
 
